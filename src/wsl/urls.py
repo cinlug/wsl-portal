d78c37d15d051af06ae3a5e555aa625a399dc67d
Adriano Melo <github@adrianomelo.com>
2011-02-03 00:33:57 -0300

    começando a codificar o template

--- src/wsl/urls.py
+++ src/wsl/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     # (r'^wsl/', include('wsl.foo.urls')),
     (r'^$', 'wsl.home.views.index'),
     (r'^programacao/', include('wsl.programacao.urls')),
+	(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root':settings.WSL_SRC_ROOT + "static"}),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
