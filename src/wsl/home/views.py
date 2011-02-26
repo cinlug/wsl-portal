b594d81a4a73d7de475c41f81702653eb341097b
Adriano Melo <github@adrianomelo.com>
2011-02-26 02:50:42 -0300

    limpando a home

--- src/wsl/home/views.py
+++ src/wsl/home/views.py
@@ -4,21 +4,4 @@ from django.conf import settings
 from wsl.programacao.models import *
 
 def index(request):
-
-	mcs = Minicurso.objects.all()
-	minicursos = []
-	
-	for mc in mcs:
-		dic = {}
-		dic["titulo"] = mc.titulo
-		dic["dia"] = mc.data.day
-		dic["mes"] = mc.data.strftime("%b")
-		dic["descricao"] = mc.descricao
-		dic["foto"] = settings.MEDIA_URL + str(mc.foto)
-		
-		palestrantes = [p.nome for p in mc.palestrante.all()]
-		dic["palestrantes"] = ", ".join(palestrantes)
-
-		minicursos.append(dic)
-	
-	return render_to_response('index.html', {'minicursos':minicursos})
+	return render_to_response('index.html')
